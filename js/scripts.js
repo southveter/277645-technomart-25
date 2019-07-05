89d9cb7d084e9247fa6a0f00f90c3bd89dd64f13
Vladimir Vetrov <southveter@gmail.com>
2019-07-05 18:38:41 +0300

    Final update

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
- <script>
 var link = document.querySelector(".feedback-button");
 if (link) {
 var popup = document.querySelector(".feedback-modal");
@@ -19,7 +18,6 @@ try {
   isStorageSupport = false;
 }
 
-
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
@@ -33,13 +31,11 @@ link.addEventListener("click", function (evt) {
   }
 });
 
-
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
 });
-
 
 form.addEventListener("submit", function (evt) {
   if (!yourName.value || !email.value || !letterText.value) {
@@ -64,14 +60,8 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
 }
-
-
-
-
-
-
-
 
 
 var mapLink = document.querySelector(".contacts-map");
@@ -100,41 +90,21 @@ window.addEventListener("keydown", function (evt) {
 }
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  var links = document.querySelectorAll(".button-catalog-item-action-buy");
+ var links = document.querySelectorAll(".button-catalog-item-action-buy");
 
 if (links) {
-  var popup = document.querySelector(".basket-modal");
-  var close = popup.querySelector(".modal-close");
-
+  var popupBasket = document.querySelector(".basket-modal");
+  var close = popupBasket.querySelector(".modal-close");
 
   links.forEach(function(item) {
     item.addEventListener("click", function (evt) {
       evt.preventDefault();
-      popup.classList.add("modal-show");
+      popupBasket.classList.add("modal-show");
     });
   });
 
-
   close.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.remove("modal-show");
+    popupBasket.classList.remove("modal-show");
   });
 }
-
-</script>
